50c9c3c1289a392d5270a9fc4f35160dfc2435a8
IceS2 <pjt1991@gmail.com>
2023-12-04 10:59:11 +0100

    Day 4

--- 2023/04/puzzle-1/src/main.rs
+++ 2023/04/puzzle-1/src/main.rs
@@ -1,3 +1,61 @@
+use std::{path::Path, fs::File, io::{self, BufRead}, collections::HashSet};
+use regex::Regex;
+
 fn main() {
-    println!("Hello World!");
+    let input = read_input("input.txt").unwrap();
+    let mut total_points: i32 = 0;
+
+    for line in input {
+        let card: Card = line.unwrap().into();
+        total_points += card.calculate_points();
+    }
+    println!("Total points: {:?}", total_points);
+}
+
+fn read_input<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[derive(Debug)]
+struct Card {
+    id: usize,
+    winning_numbers: HashSet<usize>,
+    numbers: HashSet<usize>
+}
+
+impl From<String> for Card {
+    fn from(s: String) -> Self {
+        let re = Regex::new(r"Card\s+(?<id>\d+): (?<wnumbers>[0-9\s]+)\|(?<numbers>[0-9\s]+)").unwrap();
+        let captures = re.captures(&s).unwrap();
+
+        let id: usize = captures["id"].parse().unwrap();
+        let winning_numbers:HashSet<usize> = captures["wnumbers"].trim().split(' ')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.trim().parse().unwrap())
+            .collect();
+        let numbers: HashSet<usize> = captures["numbers"].trim().split(' ')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.trim().parse().unwrap())
+            .collect();
+
+        Self {
+            id,
+            winning_numbers,
+            numbers
+        }
+    }
+}
+
+impl Card {
+    fn calculate_points(&self) -> i32 {
+        let intersection: Vec<usize> = self.winning_numbers.intersection(&self.numbers).copied().collect();
+
+        if intersection.is_empty() {
+            return 0;
+        }
+
+        2_i32.pow((intersection.len() - 1) as u32)
+    }
 }
--- 2023/04/puzzle-2/src/main.rs
+++ 2023/04/puzzle-2/src/main.rs
@@ -1,3 +1,92 @@
+use std::{path::Path, fs::File, io::{self, BufRead}, collections::{HashSet, HashMap}};
+use regex::Regex;
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello World!");
+    let input = read_input("input.txt").unwrap();
+    let mut card_stack: CardStack = CardStack::new();
+
+    for line in input {
+        let card: Card = line.unwrap().into();
+        card_stack.add(card);
+    }
+
+    card_stack.process();
+}
+
+fn read_input<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[derive(Debug)]
+struct Card {
+    id: usize,
+    winning_numbers: HashSet<usize>,
+    numbers: HashSet<usize>
+}
+
+impl From<String> for Card {
+    fn from(s: String) -> Self {
+        let re = Regex::new(r"Card\s+(?<id>\d+): (?<wnumbers>[0-9\s]+)\|(?<numbers>[0-9\s]+)").unwrap();
+        let captures = re.captures(&s).unwrap();
+
+        let id: usize = captures["id"].parse().unwrap();
+        let winning_numbers:HashSet<usize> = captures["wnumbers"].trim().split(' ')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.trim().parse().unwrap())
+            .collect();
+        let numbers: HashSet<usize> = captures["numbers"].trim().split(' ')
+            .filter(|s| !s.is_empty())
+            .map(|s| s.trim().parse().unwrap())
+            .collect();
+
+        Self {
+            id,
+            winning_numbers,
+            numbers
+        }
+    }
+}
+
+impl Card {
+    fn calculate_points(&self) -> usize {
+        let intersection: Vec<usize> = self.winning_numbers.intersection(&self.numbers).copied().collect();
+
+        intersection.len()
+    }
+}
+
+#[derive(Debug)]
+struct CardStack {
+    cards: HashMap<usize, Card>
+}
+
+impl CardStack {
+    fn new() -> Self {
+        Self {
+            cards: HashMap::new()
+        }
+    }
+
+    fn add(&mut self, card: Card) {
+        self.cards.entry(card.id).or_insert(card);
+    }
+
+    fn process(&self) {
+        let mut ref_map: HashMap<usize, usize> = self.cards.keys().map(|k| (*k, 1)).collect();
+
+        for card_id in self.cards.keys().sorted() {
+            let points = &self.cards.get(card_id).unwrap().calculate_points();
+            let card_quantity = *ref_map.get(card_id).unwrap();
+
+            for point in 1..points+1 {
+                let id = card_id + point;
+                ref_map.entry(id).and_modify(|q| *q += card_quantity);
+            }
+        }
+
+        println!("{:?}", ref_map.values().sum::<usize>());
+    }
 }
