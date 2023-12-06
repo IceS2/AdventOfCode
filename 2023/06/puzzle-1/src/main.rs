b323039338c0530b5806f09ed369cd201b7b760b
IceS2 <pjt1991@gmail.com>
2023-12-06 11:47:15 +0100

    Day 6

--- 2023/06/puzzle-1/src/main.rs
+++ 2023/06/puzzle-1/src/main.rs
@@ -1,3 +1,90 @@
+use std::{path::Path, fs::File, io::{self, BufRead}};
+
 fn main() {
-    println!("Hello World!");
+    let input: Vec<String> = read_input("input.txt").unwrap().map(|l| l.unwrap()).collect();
+
+    let times: Vec<u32> = input[0]
+        .split("Time:")
+        .last()
+        .unwrap()
+        .trim()
+        .split(' ')
+        .filter(|r| !r.is_empty())
+        .map(|n| n.parse::<u32>().unwrap())
+        .collect();
+    let distances: Vec<u32> = input[1]
+        .split("Distance:")
+        .last()
+        .unwrap()
+        .trim()
+        .split(' ')
+        .filter(|r| !r.is_empty())
+        .map(|n| n.parse::<u32>().unwrap())
+        .collect();
+
+    let races: AllRaces = AllRaces::new(times, distances);
+
+    let result: u32 = races.races.iter()
+        .map(|race| race.determine_number_of_ways_to_beat_the_record())
+        .fold(1, |acc, x| acc * x as u32);
+
+    println!("{:?}", result);
+}
+
+fn read_input<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[derive(Debug)]
+struct Race {
+    time: u32,
+    distance: u32
+}
+
+impl Race {
+    fn determine_number_of_ways_to_beat_the_record(&self) -> usize {
+        // The function that defines the distance is given by:
+        // d = ct * (t - ct) // Where d = distance, ct = charging time, t = time
+        // 0 = - ct^2 + ct * t - d
+        // ----
+        // a = -1, b = t, c = -d
+        //
+        // delta = b^2 - 4ac
+        // bhaskara = (-b +- sqrt(delta)) / 2a
+
+        let a: i32 = -1;
+        let b: i32 = self.time as i32;
+        let c: i32 = -(self.distance as i32);
+
+        let delta: f32 = (b.pow(2) - 4 * a * c) as f32;
+
+        if delta < 0.0 {
+            0
+        } else {
+            let root_1: f32 = ((-b) as f32 + delta.sqrt()) / (2 * a) as f32;
+            let root_2: f32 = ((-b) as f32 - delta.sqrt()) / (2 * a) as f32;
+            if root_1 == root_2 {
+                (0..=self.time).map(|t| t as f32).filter(|t| t >= &root_1).collect::<Vec<f32>>().len()
+            } else {
+                (0..=self.time).map(|t| t as f32).filter(|t| t >= &root_1 && t <= &root_2).collect::<Vec<f32>>().len()
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct AllRaces {
+    races: Vec<Race>
+}
+
+impl AllRaces {
+    fn new(times: Vec<u32>, distances: Vec<u32>) -> Self {
+        let races: Vec<Race> = times.iter().zip(distances.iter()).map(|(time, distance)| Race { time: *time, distance: *distance }).collect();
+
+        Self {
+            races
+        }
+    }
 }
--- 2023/06/puzzle-2/src/main.rs
+++ 2023/06/puzzle-2/src/main.rs
@@ -1,3 +1,71 @@
+use std::{path::Path, fs::File, io::{self, BufRead}};
+
 fn main() {
-    println!("Hello World!");
+    let input: Vec<String> = read_input("input.txt").unwrap().map(|l| l.unwrap()).collect();
+
+    let time: u64 = input[0]
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<String>()
+        .split("Time:")
+        .last()
+        .unwrap()
+        .parse()
+        .unwrap();
+    let distance: u64 = input[1]
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect::<String>()
+        .split("Distance:")
+        .last()
+        .unwrap()
+        .parse()
+        .unwrap();
+
+    let race: Race = Race { time, distance };
+
+    println!("{:?}", race.determine_number_of_ways_to_break_the_record());
+}
+
+fn read_input<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[derive(Debug)]
+struct Race {
+    time: u64,
+    distance: u64
+}
+
+impl Race {
+    fn determine_number_of_ways_to_break_the_record(&self) -> usize {
+        // The function that defines the distance is given by:
+        // d = ct * (t - ct) // Where d = distance, ct = charging time, t = time
+        // 0 = - ct^2 + ct * t - d
+        // ----
+        // a = -1, b = t, c = -d
+        //
+        // delta = b^2 - 4ac
+        // bhaskara = (-b +- sqrt(delta)) / 2a
+
+        let a: i64 = -1;
+        let b: i64 = self.time as i64;
+        let c: i64 = -(self.distance as i64);
+
+        let delta: f64 = (b.pow(2) - 4 * a * c) as f64;
+
+        if delta < 0.0 {
+            0
+        } else {
+            let root_1: f64 = ((-b) as f64 + delta.sqrt()) / (2 * a) as f64;
+            let root_2: f64 = ((-b) as f64 - delta.sqrt()) / (2 * a) as f64;
+            if root_1 == root_2 {
+                (0..=self.time).map(|t| t as f64).filter(|t| t >= &root_1).collect::<Vec<f64>>().len()
+            } else {
+                (0..=self.time).map(|t| t as f64).filter(|t| t >= &root_1 && t <= &root_2).collect::<Vec<f64>>().len()
+            }
+        }
+    }
 }
